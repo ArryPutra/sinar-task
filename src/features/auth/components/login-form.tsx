9f8e2026eefde6ff0d8c3a5e2c58d638711e3e69
ArryPutra <arrykusumaputra04@gmail.com>
2026-07-13 14:27:13 +0800

    inisiasi tombol login form

--- src/features/auth/components/login-form.tsx
+++ src/features/auth/components/login-form.tsx
@@ -4,8 +4,8 @@ import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
 import { Button } from "@/components/ui/button";
 import { Field, FieldGroup, FieldLabel } from "@/components/ui/field";
 import { Input } from "@/components/ui/input";
+import { Separator } from "@/components/ui/separator";
 import { Spinner } from "@/components/ui/spinner";
-import { initialActionState } from "@/types/action-state";
 import { InfoIcon } from "lucide-react";
 import { useActionState } from "react";
 import { loginAction } from "../actions";
@@ -63,6 +63,22 @@ export default function LoginForm() {
                                 Login {isPending && <Spinner />}
                             </Button>
                         </Field>
+                        <div className="flex w-full items-center">
+                            <Separator className="flex-1" />
+                            <span className="mx-4 shrink-0 text-sm text-muted-foreground">
+                                Atau lanjut dengan
+                            </span>
+                            <Separator className="flex-1" />
+                        </div>
+                        <Button variant="outline" type="button">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                                <path
+                                    d="M12.48 10.92v3.28h7.84c-.24 1.84-.853 3.187-1.787 4.133-1.147 1.147-2.933 2.4-6.053 2.4-4.827 0-8.6-3.893-8.6-8.72s3.773-8.72 8.6-8.72c2.6 0 4.507 1.027 5.907 2.347l2.307-2.307C18.747 1.44 16.133 0 12.48 0 5.867 0 .307 5.387.307 12s5.56 12 12.173 12c3.573 0 6.267-1.173 8.373-3.36 2.16-2.16 2.84-5.213 2.84-7.667 0-.76-.053-1.467-.173-2.053H12.48z"
+                                    fill="currentColor"
+                                />
+                            </svg>
+                            Login with Google
+                        </Button>
                     </FieldGroup>
                 </form>
                 <span className="font-medium tracking-wide text-xs text-gray-700 max-md:block">
